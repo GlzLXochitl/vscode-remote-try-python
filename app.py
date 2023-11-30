394167b5e6ba3dd9e3860e6efb5646f753382e7f
GlzLXochitl <124531967+GlzLXochitl@users.noreply.github.com>
2023-11-30 04:30:17 +0000

    juego finalizado con una interfas simple

--- app.py
+++ app.py
@@ -10,5 +10,20 @@ app = Flask(__name__)
 def hello():
     return app.send_static_file("index.html")
 
-# write 'hello world' to the console
-print("hello world")
+import random
+
+opciones = ['piedra', 'papel', 'tijera']
+
+eleccion_usuario = input("Elige piedra, papel o tijera: ")
+eleccion_computadora = random.choice(opciones)
+
+print("La computadora eligió: " + eleccion_computadora)
+
+if eleccion_usuario == eleccion_computadora:
+    print("Es un empate")
+elif (eleccion_usuario == 'piedra' and eleccion_computadora == 'tijera') or \
+     (eleccion_usuario == 'papel' and eleccion_computadora == 'piedra') or \
+     (eleccion_usuario == 'tijera' and eleccion_computadora == 'papel'):
+    print("¡Ganaste!")
+else:
+    print("Perdiste")
